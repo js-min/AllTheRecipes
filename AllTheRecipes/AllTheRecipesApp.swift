d950a6d9459d94f03fa6f6fd4c856065471a388b
js-min <alswltn1@korea.ac.kr>
2022-05-12 22:59:08 +0900

    Initial Commit

---
+++ AllTheRecipes/AllTheRecipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  AllTheRecipesApp.swift
+//  AllTheRecipes
+//
+//  Created by JISU MIN on 2022/05/12.
+//
+
+import SwiftUI
+
+@main
+struct AllTheRecipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
